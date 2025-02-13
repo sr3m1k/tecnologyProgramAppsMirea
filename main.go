896c58d67cbc2e14713421608dc7d90b12756f36
kx1ned <progres.krst@mail.ru>
2025-02-13 11:14:25 +0300

    MY FIRST COMMIT I DAYN TYPOI

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const maxCount = 15
+	var number int
+
+	fmt.Printf("Введите количество элементов массива (максимум %d): ", maxCount)
+	fmt.Scan(&number)
+
+	if number > maxCount {
+		fmt.Printf("Количество элементов не может превышать %d\n", maxCount)
+		return
+	}
+
+	numArray := make([]int, number)
+
+	fmt.Println("Введите элементы массива:")
+	for i := 0; i < number; i++ {
+		fmt.Scan(&numArray[i])
+	}
+
+	fmt.Println("Введенный массив:")
+	for _, v := range numArray {
+		fmt.Print(v, " ")
+	}
+	fmt.Println()
+}
